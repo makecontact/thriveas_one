f47a994bb8314b72113099ecbbd5deb4f7870c8d
Matthew Shelley <matt@mjshelley.com>
2021-07-28 10:39:23 +0000

    first commit

---
+++ thriveasone.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Thrive As One 
+ * Description:       Custom libraries and code for the Thrive as one website.
+ * Version:           1.0.0
+ * Author:            MJ Shelley
+ * Author URI:        https://mjshelley.com/
+ * Author Email:      matthew@mjshelley.com
+ * Plugin URI: 		  https://thriveasone.ca
+ * Text Domain:       mattshelley
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+
+?>
